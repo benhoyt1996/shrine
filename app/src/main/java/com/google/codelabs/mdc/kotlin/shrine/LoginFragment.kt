672a9cbaf6f1164f23cad9009d04974d6d81febd
3807554 <philip.hoyt@fedex.com>
2019-08-08 14:32:42 -0500

    finished mdc-101

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/LoginFragment.kt
@@ -2,9 +2,13 @@ package com.google.codelabs.mdc.kotlin.shrine
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.shr_login_fragment.*
 import kotlinx.android.synthetic.main.shr_login_fragment.view.*
@@ -27,21 +31,35 @@ class LoginFragment : Fragment() {
             } else {
                 //Clear password error
                 password_text_input.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
             }
         }
 
         // Snippet from "Navigate to the next Fragment" section goes here.
 
-        view.password_edit_text.setOnKeyListener { _, _, _ ->
-            if(isPasswordValid(password_edit_text.text!!)) {
-                password_text_input.error = null
-            }
-            false
-        }
 
+        view.password_edit_text.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Do Nothing.
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Do Nothing.
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (isPasswordValid(password_edit_text.text!!)) {
+                    // Clear the error.
+                    password_text_input.error = null
+                }
+            }
+
+        })
 
         return view
     }
+
+
 
 
 
@@ -51,6 +69,7 @@ class LoginFragment : Fragment() {
 
     // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
     private fun isPasswordValid(text: Editable?): Boolean {
+        Log.d("test log: ", "")
         return text != null && text.length >= 8
 
     }
